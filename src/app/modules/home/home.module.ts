646c117e8d1b676965695390d1c3adcd2289f714
Hector Romero <hectorromerodev@gmail.com>
2021-10-07 04:41:55 -0700

    UPD home module

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    HomePageComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
