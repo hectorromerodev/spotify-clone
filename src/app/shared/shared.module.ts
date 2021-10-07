bdbd79af112624c2d8230318a98bbe753f8b989d
Hector Romero <hectorromerodev@gmail.com>
2021-10-07 04:33:30 -0700

    UPD shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { UserHeaderComponent } from './components/user-header/user-header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SideBarComponent,
+    UserHeaderComponent
+  ],
   imports: [
     CommonModule
   ]
